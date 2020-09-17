755ca8bcaa72408ebfc41a2ef6de1c59907cef24
Tom.Yu <569721086@qq.com>
2020-09-17 17:09:55 +0800

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-wechat-pay "0.1.0"
+(defproject clj-wechat-pay "0.1.1"
   :description "clojure wechat pay"
   :url "https://github.com/ZZGit/clj-wechat-pay"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
